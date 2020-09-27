855322462817fb9d11a63ead54e3f7740b98638a
zajanah <zajanah08@gmail.com>
2020-09-27 14:23:44 +0100

    heroku commit

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+app.use(express.static('./dist/{{github-followers}}'));
 
-app.use(express.static(__dirname + '/dist/github-followers'));
+app.get('/*', function (req, res) {
 
-app.all('*', (req, res) => {
-    res.status(200).sendFile(__direname + '/dist/github-followers/index.html');
-
+    res.sendFile(path.join(__dirname, '/dist/{{github-followers}}/index.html'));
 });
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
